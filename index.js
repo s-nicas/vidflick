70aba544357b90d8e976dcae399dc489d73de766
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-02-18 10:43:20 -0800

    Add home, genres, and genre update routes along with validate genre function

--- index.js
+++ index.js
@@ -5,10 +5,38 @@ const app = express();
 
 app.use(express.json())
 
+const genres = [{id: 1, name: 'scary'}, {id:2, name: 'flick'}]
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
 
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
 app.get('/api/genres', (req, res) => {
   res.send(genres)
 })
+
+app.post('/api/genres', (req, res) => {
+  const {error} = validateGenre(req.body)
+
+  if(error) return res.status(400).send(error.details[0].message)
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  }
+
+  genres.push(genre)
+  res.send(genre)
+})
+
+
+function validateGenre(course){
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+  return Joi.validate(course, schema)
+}

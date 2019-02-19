468d15d63e295314062426a98208e4fd785e5e5e
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-02-18 16:34:55 -0800

    Refractored code - moved out all routes and imported modules

--- index.js
+++ index.js
@@ -1,77 +1,15 @@
 const express = require('express')
 const Joi = require('joi')
+const genres = require('./routes/genres')
+const home = require('./routes/home')
 
 const app = express();
-
+// will parse the request to json - and give us access to req.body
 app.use(express.json())
+app.use('/api/genres', genres)
+app.use('/', home)
 
-const genres = [{id: 1, name: 'scary'}, {id:2, name: 'flick'}]
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome')
-})
-
-app.get('/api/genres', (req, res) => {
-  res.send(genres)
-})
-
-app.post('/api/genres', (req, res) => {
-  const {error} = validateGenre(req.body)
-
-  if(error) return res.status(400).send(error.details[0].message)
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  }
-
-  genres.push(genre)
-  res.send(genre)
-})
-
-app.get('/api/genres/:id', (req, res) => {
-  const genre = setGenre(req.params.id)
-
-  if(!genre) return res.status(404).send('The genre with the give ID was not found')
-
-  res.send(genre)
-})
-
-app.put('/api/genres/:id', (req, res) => {
-  const genre = setGenre(req.params.id)
-
-  if(!genre) return res.status(404).send('The genre with the give ID was not found')
-
-  const {error} = validateGenre(req.body)
-
-  if(error) return res.status(400).send(error.details[0].message)
-
-  genre.name = req.body.name
-  res.send(genre)
-})
-
-app.delete('/api/genres/:id', (req, res) => {
-  const genre = setGenre(req.params.id)
-
-  if(!genre) return res.status(404).send('The genre with the give ID was not found')
-
-  const index = genres.indexOf(genre)
-  genres.splice(index, 1)
-  res.send(genre)
-})
-
-
-function validateGenre(course){
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
-  return Joi.validate(course, schema)
-}
-
-function setGenre(genreId){
-  return genres.find( genre => genre.id === parseInt(genreId))
-}

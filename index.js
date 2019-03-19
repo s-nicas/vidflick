b7467d94d07bca6f223a19ae1732705609338b8d
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-19 12:44:52 -0700

    Add customer
    required Customer model, add route

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const Joi = require('joi')
 const genres = require('./routes/genres')
+const movies = require('./routes/customers')
 const home = require('./routes/home')
-
+const mongoose = require('mongoose')
 const app = express();
+
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB ...', err))
+
+
 // will parse the request to json - and give us access to req.body
 app.use(express.json())
 app.use('/api/genres', genres)
+app.use('/api/movies', movies)
 app.use('/', home)
 
 

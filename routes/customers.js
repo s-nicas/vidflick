c3dce3c1944a9e8678a243011987cf7cb15e8917
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-19 13:50:00 -0700

    Add update and delete customer route

--- routes/customers.js
+++ routes/customers.js
@@ -48,6 +48,25 @@ router.get('/:id', async (req, res) => {
   res.send(customer)
 })
 
+router.put('/:id', async (req, res) => {
+  const { error } = validateCustomer(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  let customer = await Customer.findByIdAndUpdate(req.params.id, {name: req.body.name, isGold: req.body.isGold, phone: req.body.phone}, {new: true})
+
+  if(!customer) return res.status(400).send('The customer with the given ID was not found.')
+
+  res.send(customer)
+})
+
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id)
+
+  if(!customer) return res.status(400).send('The customer with the given ID was not found.')
+
+  res.send(customer)
+})
+
 
 function validateCustomer(customer) {
   const schema = {

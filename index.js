f69b490d4090ba97f7f298fa4a95a472231fa030
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-19 19:26:38 -0700

    Fix but in rental route
    removed '.' in app.use rentals

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 const home = require('./routes/home')
 
 
@@ -19,6 +20,7 @@ app.use(express.json())
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
+app.use('/api/rentals', rentals)
 app.use('/', home)
 
 

8fa6f0fbf36d0d2d78898c47a409f4a1f35f2bd1
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-19 18:12:34 -0700

    Add movie model, movie schema & movie validation

---
+++ models/movie.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const { genreSchema } = require('./genre')
+
+const Movie = mongoose.model('Movie', new mongoose.Schema({
+  // representation of our modal - what we will store in MongoDB
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 10
+    },
+    genre: {
+      type: genreSchema,
+      required: true
+    },
+
+    numberInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 200
+    },
+    dailyRentalRate: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 200
+    }
+  })
+)
+
+// what the clien sends us - input to API
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().min(3).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).max(200).required(),
+    dailyRentalRate: Joi.number().min(0).max(200).required()
+  };
+
+  return Joi.validate(movie, schema);
+}
+
+exports.Movie = Movie
+exports.validate = validateMovie

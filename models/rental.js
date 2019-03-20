cda5d77c1e729a46d0681df1576f7c42947b8ecf
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-20 10:38:48 -0700

    Update rental schema
    created a new mongoose schema for customer and movie instead of using exsisting schema. Prevents bloat should movie and rentals grow

--- models/rental.js
+++ models/rental.js
@@ -1,29 +1,70 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
-const { customerSchema } = require('./customer')
-const { movieSchema } = require('./movie')
-
 
 const rentalSchema = ({
   customer: {
-    type: customerSchema,
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      }
+    }),
     required: true
   },
   movie: {
-    type: movieSchema,
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
     required: true
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dateReturned: {
+    type: Date
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
   }
-})
+});
+
 
 const Rental = mongoose.model('Rental', rentalSchema)
 
 function validateRental(rental){
   const schema = {
     customerId: Joi.string().required(),
-    movieId: Joi.String().required()
+    movieId: Joi.string().required()
   }
   return Joi.validate(rental, schema)
 }
 
 exports.Rental = Rental
-exports.validate = validateRental
+exports.validateRental = validateRental

91fe898c98c7720599a7ab8946d5fbea04b283d7
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-21 16:50:31 -0700

    Add user module and route

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const home = require('./routes/home')
+const users = require('./routes/users')
 
 
 
@@ -23,6 +24,7 @@ app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
+app.use('/api/users', users)
 app.use('/', home)
 
 

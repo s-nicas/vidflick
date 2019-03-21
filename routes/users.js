d63ef11a45b2007294c91d6a38a9b990213151f6
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-21 16:51:27 -0700

    Add user route - will be user sign up route

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const { User, validate} = require('../models/user')
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body)
+  if (error) res.status(400).send(error.details[0].message)
+
+  let user = await User.findOne({email: req.body.email})
+  if (user) return res.status(400).send('User already registered')
+
+  user = new User({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email
+  })
+
+  await user.save()
+  res.send(user)
+})
+
+module.exports = router

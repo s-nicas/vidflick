961733ef392d558c40123718fcd1e8a271201353
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-22 14:52:50 -0700

    Add auth middlewared to post route

--- routes/genres.js
+++ routes/genres.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const { Genre, validate } = require('../models/genre')
+const auth = require('../middleware/auth')
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
+
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 

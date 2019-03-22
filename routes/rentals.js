8d072ee297eec4ade3a747d019c98716fd994012
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-22 14:56:41 -0700

    Add auth to new route

--- routes/rentals.js
+++ routes/rentals.js
@@ -5,6 +5,7 @@ const { Rental, validateRental } = require('../models/rental')
 const { Customer } = require('../models/customer')
 const { Movie } = require('../models/movie')
 const Fawn = require('fawn')
+const auth = require('../middleware/auth')
 
 Fawn.init(mongoose)
 
@@ -13,7 +14,7 @@ router.get('/', async (req, res) => {
   res.send(rentals)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validateRental(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
@@ -48,8 +49,7 @@ router.post('/', async (req, res) => {
       .run()
       res.send(rental)
   }
-
-  catch(ex){
+    catch(ex){
     res.status(500).send('Something Failed')
   }
 

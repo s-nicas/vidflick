4520d4ead694637563c3c79287eee4a94d322e6b
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-21 17:19:07 -0700

    Add bcrypt and encrypt password

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const { User, validate} = require('../models/user')
+const _ = require('lodash')
+const bcrypt = require('bcrypt')
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body)
@@ -10,14 +12,13 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({email: req.body.email})
   if (user) return res.status(400).send('User already registered')
 
-  user = new User({
-    name: req.body.name,
-    password: req.body.password,
-    email: req.body.email
-  })
+  user = new User(_.pick(req.body, ['name', 'email', 'password']))
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
 
   await user.save()
-  res.send(user)
+
+  res.send(_.pick(user, ['_id','name', 'email']))
 })
 
 module.exports = router

e0d091ccd5ed6b06e661619b1b91c866c355d77b
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-22 15:35:59 -0700

    Add admin verification to delete generes

--- routes/genres.js
+++ routes/genres.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { Genre, validate } = require('../models/genre')
 const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -33,7 +34,7 @@ router.put('/:id', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', auth,  async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');

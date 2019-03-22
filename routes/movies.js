e4be496b03e640fcc480ec6346d607fbb0ed0f61
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-22 14:55:35 -0700

    Add auth to new, update and delete route

--- routes/movies.js
+++ routes/movies.js
@@ -3,13 +3,14 @@ const express = require('express');
 const router = express.Router();
 const { Movie, validate } = require('../models/movie')
 const { Genre } = require('../models/genre')
+const auth = require('../middleware/auth')
 
 router.get('/', async(req, res) => {
   const movies = await Movie.find().sort('title')
   res.send(movies)
 })
 
-router.post('/', async(req, res) => {
+router.post('/', auth, async(req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
@@ -29,7 +30,7 @@ router.get('/:id', async(req, res) => {
   res.send(movie)
 })
 
-router.put('/:id', async(req, res) => {
+router.put('/:id', auth, async(req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
@@ -41,7 +42,7 @@ router.put('/:id', async(req, res) => {
 
 })
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', auth, async(req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id);
 
   if (!movie) return res.status(404).send('The movie with the given ID was not found.');

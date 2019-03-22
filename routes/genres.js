70ae2af280755c4d24ae3f42a742134f93893501
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-03-22 14:54:18 -0700

    Add auth middleware to update and delete routes

--- routes/genres.js
+++ routes/genres.js
@@ -20,7 +20,7 @@ router.post('/', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -33,7 +33,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth,  async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');

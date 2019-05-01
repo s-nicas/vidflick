c1db7695be57f6dd7d857d57431285f70ba7f0f4
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-04-30 22:35:13 -0700

    Add process.exit 1

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+const config = require('config')
 const Joi = require('Joi')
 Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose')
-const express = require('express')
-const app = express();
-
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const home = require('./routes/home')
+const auth = require('./routes/auth')
 const users = require('./routes/users')
+const express = require('express')
+const app = express();
+const error = require('./middleware/error')
+require('express-async-errors')
 
 
+ if (!config.get('jwtPrivateKey')){
+   console.error('FATAL ERROR: jwtPrivateKey is not defined')
+   process.exit(1);
+ }
 
 mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
@@ -25,7 +32,10 @@ app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('api/auth', auth)
 app.use('/', home)
+
+app.use(error)
 
 
 
